6510ab4d227f9f981ec3aaf4f114872873d960d9
north1013 <north1013@github.com>
2018-11-21 14:36:58 +0100

    grunderna

---
+++ onsdag.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+foo = "Det här är en string"
+bar = 98 # Det här är en integer
+foobar = 0.2 # Det här är en float
+
+print(foo, bar) # Printar ut värdet av variablern foo och bar
+
+foo_bar = str(bar) # Det här konverterar bar/12 till en string
+
+print(foo_bar)
+
+print(chr(98)) # Det här konverterar integer till en unicode karaktär
+print(ord("a")) # Det här konverterar en unicode karaktär till en integer
